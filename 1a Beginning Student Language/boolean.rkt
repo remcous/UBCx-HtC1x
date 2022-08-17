99dd4ab6b299cd14128df6a327c7cf4bdc2ac5fe
Remi Coussement <remcous@gmail.com>
2022-08-16 21:55:06 -0700

    Initial Commit

---
+++ 1a Beginning Student Language/boolean.rkt
@@ -0,0 +1,23 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname boolean) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+
+;true
+;false
+
+(define WIDTH 100)
+(define HEIGHT 100)
+
+;(> WIDTH HEIGHT)
+;(>= WIDTH HEIGHT)
+
+;(string=? "foo" "bar")
+
+(define I1 (rectangle 10 20 "solid" "red"))
+(define I2 (rectangle 20 10 "solid" "blue"))
+
+(if (< (image-width I1)
+       (image-height I1))
+    "tall"
+    "wide")
